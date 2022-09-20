9d524b7c5e17a54ef49b4922374ec0ea4411769a
Сергей <forgedwheels@gmail.com>
2022-09-20 14:31:04 +0300

    First commit

---
+++ API/PARSER/config_example.py
@@ -0,0 +1,11 @@
+# данные для подключения к БД Ecom Seller (market_db)
+DB_DSN = """
+host = rc1b-itt1uqz8cxhs0c3d.mdb.yandexcloud.net
+port = 6432
+dbname = market_db
+user = your_username_here
+password = your_password_here
+target_session_attrs = read-write
+sslmode = verify-full
+sslrootcert=root.crt
+"""
