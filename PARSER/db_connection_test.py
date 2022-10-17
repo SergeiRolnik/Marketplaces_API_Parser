9a58dbdbdca09018dfcbdfd67eadc40dfc821cf5
Сергей <forgedwheels@gmail.com>
2022-10-17 15:30:14 +0300

    final version

--- PARSER/db_connection_test.py
+++ PARSER/db_connection_test.py
@@ -7,5 +7,10 @@ try:
     print("Информация о сервере PostgreSQL")
     for key, value in connection.get_dsn_parameters().items():
         print(key, value)
+    sql = "SELECT table_name FROM information_schema.tables WHERE (table_schema = 'public')"
+    cursor.execute(sql)
+    print('Список доступных таблиц')
+    for table in cursor.fetchall():
+        print(table)
 except Exception as error:
     print('Connection error', error)
